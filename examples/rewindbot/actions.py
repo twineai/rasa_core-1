daf831a5a6f52382590c24bf537636a6b54ae867
Josh Converse <josh.converse@gmail.com>
2018-01-25 12:54:15 -0800

    Simplify

--- examples/rewindbot/actions.py
+++ examples/rewindbot/actions.py
@@ -6,12 +6,21 @@ from __future__ import unicode_literals
 from rasa_core.actions.action import Action
 from rasa_core.events import SlotSet, UserUtteranceReverted
 
-class ActionDoDestructiveThing(Action):
+class ActionDoDestructiveThingOne(Action):
     def name(self):
-        return 'action_do_destructive_thing'
+        return 'action_do_destructive_thing_one'
 
     def run(self, dispatcher, tracker, domain):
-        dispatcher.utter_message("Doing a destructive thing")
+        dispatcher.utter_message("Doing destructive thing one")
+        return []
+
+
+class ActionDoDestructiveThingTwo(Action):
+    def name(self):
+        return 'action_do_destructive_thing_two'
+
+    def run(self, dispatcher, tracker, domain):
+        dispatcher.utter_message("Doing destructive thing two")
         return []
 
 
--- examples/rewindbot/stories.md
+++ examples/rewindbot/stories.md
@@ -1,11 +1,8 @@
 ## greet
 * do_destructive_thing
-    - action_do_destructive_thing
+    - action_do_destructive_thing_one
+    - action_do_destructive_thing_two
 
 ## do thing
 * do_inconsequential_thing
     - action_do_inconsequential_thing
-
-## goodbye
-* goodbye
-    - utter_goodbye
